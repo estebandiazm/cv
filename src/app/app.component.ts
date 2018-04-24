6d29dc347c38465727f1225d367c96a2fd37b50c
Juan Esteban Diaz <jdiaz@easysol.net>
2018-04-24 16:02:23 -0500

    Introduction profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   name = 'Juan Esteban Díaz Montejo';
+  profile = 'Ingeniero de Sistemas y Computación de la Universidad Pedagógica y Tecnológica de Colombia. Apacionado por la innvovación y las nuevas tecnologías en el desarrollo de software. Convencido del trabajo en equipo y la cooperación como pilar para el exito de cualquier proyecto. Con experencia en trabajo equipo de manera remota y presencial, y con altas capacidades de comunicación interpersonal.'
 }
